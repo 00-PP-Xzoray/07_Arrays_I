5b572ee9d21bcbc290520e9860b641be1a0e5096
Sohrab <sohrab-arenja8@hotmail.de>
2023-07-19 10:24:55 +0200

    Array

--- script_01.js
+++ script_01.js
@@ -8,9 +8,13 @@
  */
 
 /*** 02. Theorie: Array */
-
-
-
+// let arr;
+// arr = new Array(); // Konstruktor
+// arr = [2, 8, 12, 20, 100]; // Literal
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Position im Array)
+// output(arr[arr.length-1]); // Letzte Position im Array
 
 /**
  *   PRAXIS
@@ -20,19 +24,48 @@
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
-// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","der","coole","Sohrab","Arenja"));
 
-function getSentence(word1,word2,word3,word4) {
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
-                PUNCT;
-
-    return str;
+                word5 + GAP +
+                word6 + PUNCT;
+return str;
 }
+
+// *** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentence("Ich","bin","der","coole","Sohrab","Arenja"));
+
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] + PUNCT;
+return str;
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -42,5 +75,3 @@ function getSentence(word1,word2,word3,word4) {
 function output(outputStr) {
     console.log(outputStr);
 }
-
-
